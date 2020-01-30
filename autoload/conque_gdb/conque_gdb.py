549fb83881c33df999893071b541666b947c1207
f1reflyyyylmao <17353799+f1reflyyyylmao@users.noreply.github.com>
2020-01-30 13:04:58 +0100

    Updated module import for newer python3 versions

--- autoload/conque_gdb/conque_gdb.py
+++ autoload/conque_gdb/conque_gdb.py
@@ -1,4 +1,4 @@
-import re, collections
+import re, collections.abc
 
 # Marks that a breakpoint has been hit
 GDB_BREAK_MARK = '\x1a\x1a'
@@ -36,7 +36,7 @@ class RegisteredBreakpoint:
     def __str__(self):
         return self.filename + ':' + self.lineno + ',' + self.enabled
 
-class RegisteredBpDict(collections.MutableMapping):
+class RegisteredBpDict(collections.abc.MutableMapping):
     def __init__(self):
         self.r_breaks = dict()
         self.lookups = dict()
